2ff4bf30a14a7d84c90397320dea82641af83795
InfiniteImperm <84783592+InfiniteImperm@users.noreply.github.com>
2021-05-25 14:55:01 +0200

    Create rr.js
    frame, content comes later

---
+++ forestbot-minecraft/commands/rr.js
@@ -0,0 +1,20 @@
+const { cooldown, bot, sleep } = require('../index.js');
+const { coolDownTime, spam } = require('../config.json')
+module.exports = function p(username, sender, flags, args) {
+    return new Promise(async (resolve, reject) => {
+        if (cooldown.has(bot.username)) {
+            await sleep(1000);
+            bot.whisper(username, spam);
+        } else {
+            await sleep(600);
+            
+            bot.chat("test")
+            resolve()
+            cooldown.add(bot.username);
+            setTimeout(() => {
+                // Removes the user from the set after a minute
+                cooldown.delete(bot.username);
+            }, coolDownTime);
+        };
+    });
+};
